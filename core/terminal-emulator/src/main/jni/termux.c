bb6db50b482431d3a84a437e2a3a74b12cbdf16c
jeffusion <admin@jeffusion.cc>
2026-03-19 14:33:36 +0800

    perf: set controlling tty for subprocess pty

--- core/terminal-emulator/src/main/jni/termux.c
+++ core/terminal-emulator/src/main/jni/termux.c
@@ -79,6 +79,7 @@ static int create_subprocess(JNIEnv* env,
 
         int pts = open(devname, O_RDWR);
         if (pts < 0) exit(-1);
+        ioctl(pts, TIOCSCTTY, 0);
 
         dup2(pts, 0);
         dup2(pts, 1);
